abc2dcd1abe8f42cf9abb0d4347e694777ddc9fe
kpdzhnv <nic.a@mail.ru>
2023-04-30 11:59:44 +0300

    Day & Night is ready!

--- Assets/Lighting/LightingManager.cs
+++ Assets/Lighting/LightingManager.cs
@@ -6,16 +6,18 @@ public class LightingManager : MonoBehaviour
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private Material Sky_material;
     [SerializeField] private LightingPreset Preset;
+
+    int DayLength = 24;
     //Variables
-    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    [SerializeField] private float TimeOfDay;
 
 
     private void Update()
     {
         //(Replace with a reference to the game time)
         TimeOfDay += Time.deltaTime;
-        TimeOfDay %= 24; //Modulus to ensure always between 0-24
-        UpdateLighting(TimeOfDay / 24f);
+        TimeOfDay %= DayLength; //Modulus to ensure always between 0-DayLength
+        UpdateLighting(TimeOfDay / DayLength);
     }
 
 
@@ -25,16 +27,25 @@ public class LightingManager : MonoBehaviour
         //RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
         //RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
-        //rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
-        DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
-        Sky_material.SetColor("_SkyTint", Preset.SkyBox_SkyColor.Evaluate(timePercent));
-
-        //var t = (timePercent + 0.5f);
-        //t = t > 1 ? t - 1 : t;
-        //DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3(Mathf.Abs(t * 180f), 170f, 0));
-
-        DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
-
+        // day
+        if (timePercent <= 0.5)
+        {
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f), 20f, 0));
+            
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f), 20f, 0));
+            float colorPercent = timePercent * 2.0f;
+            DirectionalLight.color = Preset.DirectionalColor_Day.Evaluate(colorPercent);
+            Sky_material.SetColor("_SkyTint", Preset.SkyBox_SkyColor_Day.Evaluate(colorPercent));
+        }
+        // night
+        else
+        {
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3(((timePercent - 0.5f) * 360f), 20f, 0));
+            float colorPercent = (timePercent - 0.5f) * 2.0f;
+            
+            DirectionalLight.color = Preset.DirectionalColor_Night.Evaluate(colorPercent);
+            Sky_material.SetColor("_SkyTint", Preset.SkyBox_SkyColor_Night.Evaluate(colorPercent));
+        }
     }
 
 }
