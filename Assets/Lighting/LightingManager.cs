eaf3351e6758aef8537cf5406dfd3cb49f9c5f4d
kpdzhnv <nic.a@mail.ru>
2023-05-01 18:04:58 +0300

    Made things pretty

--- Assets/Lighting/LightingManager.cs
+++ Assets/Lighting/LightingManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class LightingManager : MonoBehaviour
 {
@@ -10,7 +11,15 @@ public class LightingManager : MonoBehaviour
     int DayLength = 24;
     //Variables
     [SerializeField] private float TimeOfDay;
+    [SerializeField] private VisualEffect FireVFX;
+    [SerializeField] private bool fire = false;
 
+    private void Start()
+    {
+        FireVFX.Play();
+        FireVFX.Stop();
+        fire = false;
+    }
 
     private void Update()
     {
@@ -23,23 +32,33 @@ public class LightingManager : MonoBehaviour
 
     private void UpdateLighting(float timePercent)
     {
-        //Set ambient and fog
-        //RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
-        //RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
-
         // day
-        if (timePercent <= 0.5)
+        if (timePercent <= 0.5f)
         {
+            if (fire)
+            {
+                fire = !fire;
+                FireVFX.Stop();
+                DirectionalLight.intensity = 2;
+            }
+
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f), 20f, 0));
             
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f), 20f, 0));
             float colorPercent = timePercent * 2.0f;
             DirectionalLight.color = Preset.DirectionalColor_Day.Evaluate(colorPercent);
             Sky_material.SetColor("_SkyTint", Preset.SkyBox_SkyColor_Day.Evaluate(colorPercent));
+            Sky_material.SetFloat("_SunSize", (Mathf.Abs(timePercent * 4.0f - 1.0f)) * 0.05f + 0.04f);
         }
         // night
         else
         {
+            if (!fire)
+            {
+                fire = !fire;
+                FireVFX.Play();
+                DirectionalLight.intensity = 1;
+            }
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3(((timePercent - 0.5f) * 360f), 20f, 0));
             float colorPercent = (timePercent - 0.5f) * 2.0f;
             
